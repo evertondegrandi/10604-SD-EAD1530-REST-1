79c20aaf8ab71bf516d083f47f3c5772c991a314
evertondegrandi <evertondegrandi@gmail.com>
2019-08-18 12:31:27 -0300

    Parte do Servidor com REST da consulta de pedidos
    Parte do servidor

---
+++ pas/repository/UPedidoRepositoryIntf.pas
@@ -0,0 +1,19 @@
+unit UPedidoRepositoryIntf;
+
+interface
+
+uses
+  UPizzaSaborEnum, UPizzaTamanhoEnum;
+
+type
+  IPedidoRepository = interface(IInterface)
+    ['{76A94FF6-4634-4C52-91E4-3F969389D917}']
+
+    procedure efetuarPedido(const APizzaTamanho: TPizzaTamanhoEnum; const APizzaSabor: TPizzaSaborEnum; const AValorPedido: Currency;
+      const ATempoPreparo: Integer; const ACodigoCliente: Integer; const ADocumentoCliente: Integer);
+//    function consultaPedido(const ACodigoCliente: Integer) : string;
+  end;
+
+implementation
+
+end.
